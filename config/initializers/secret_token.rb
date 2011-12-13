a35e7ac19402ef03e1c016138314e2dac6cd8d4d
Kyle Rogers <kyle.rogers@gmail.com>
2011-12-12 21:12:37 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Core3pm::Application.config.secret_token = 'bb6be98d1d749ecb81886d0863eb88ee7ec7ef05493535706a5cb803ab7a575d21ff215ec9c6a2727e41d7389692e54652ba69b2b927502b47fd4799c56700ef'
