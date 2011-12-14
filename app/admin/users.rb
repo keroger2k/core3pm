23af2395b47bbe15bf6506653cbb77ff056a126b
Kyle Rogers <kyle.rogers@gmail.com>
2011-12-14 16:04:52 -0600

    added active admin.  updated default index views

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :name
+    column :email
+    column :available
+    column :returns
+    column :message
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
 end
