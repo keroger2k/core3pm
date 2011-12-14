6ef97760f0332cd78e8dc2dfd5c201559d1df243
Kyle Rogers <kyle.rogers@gmail.com>
2011-12-14 15:33:03 -0600

    adding users to admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Core3pm::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   get "users/index"
   devise_for :users
   root :to => 'users#index'
