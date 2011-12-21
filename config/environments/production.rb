c963e2b7b299af8d32ffb99b9b47c0a63fc498b8
Kyle Rogers <kyle.rogers@gmail.com>
2011-12-21 08:49:24 -0600

    setting up production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Core3pm::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.action_mailer.default_url_options = {:host => 'afternoon-cloud-1590.herokuapp.com'}
 
   # Generate digests for assets URLs
   config.assets.digest = true
